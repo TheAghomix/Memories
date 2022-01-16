03d2d326e7742bad7ab4a24cf12c985871413e77
Aghomix <aghomix@gmail.com>
2022-01-16 14:26:57 -0800

    added jwt  and added user model

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
-app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true })) 
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://solo:aolo@cluster0.antis.mongodb.net/memories?retryWrites=true&w=majority';
+// const CONNECTION_URL = 'mongodb://localhost:27017/memories'
+
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
